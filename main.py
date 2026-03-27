6ee496963faa29b12969a5f2cb40e6a0d1459220
Mohamed Nasser <nassermohamed3222@gmail.com>
2026-03-27 17:57:37 +0200

    return to real paths

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from trainer import Trainer
 
 
 if __name__ == "__main__":
-    trainer = Trainer(config_path="kaggle/working/MMI/configs/unet_ct_exp.json")
+    trainer = Trainer(config_path="configs/unet_ct_exp.json")
     trainer.fit()
