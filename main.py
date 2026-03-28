b2b849cba1b04cec9f8d8b542d999f535f924e14
Mohamed Nasser <nassermohamed3222@gmail.com>
2026-03-28 11:48:07 +0200

    decrease batch size to 16

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from trainer import Trainer
 import wandb
 
-wandb.login(key="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")
+# wandb.login(key="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")
 
 if __name__ == "__main__":
     trainer = Trainer(config_path="configs/unet_ct_exp.json")
