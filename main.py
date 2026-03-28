4f3e8fd8dab392b042857a6290a787024806729c
MohamedAlaaAli <muhammed.alaa157@gmail.com>
2026-03-28 05:56:43 +0200

    add api key for wandb

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from trainer import Trainer
+import wandb
 
+wandb.login(key="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")
 
 if __name__ == "__main__":
+
     trainer = Trainer(config_path="configs/unet.json")
     trainer.fit()
